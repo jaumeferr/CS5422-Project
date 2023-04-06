ef0af873d28402268264c5b301263db84d9b5752
Aldo-Aditiya <aldoan100@gmail.com>
2023-04-06 21:19:06 +0700

    Script to populate podcast db

---
+++ lib/2023-04-06_generate_test_data.py
@@ -0,0 +1,10 @@
+from dataset import PodcastDataset, PodcastDB
+
+podcast_db = PodcastDB(db_name="test_database")
+podcast_dataset = PodcastDataset(base_dir="podcast_details/", database=podcast_db)
+
+podcast_url_list = ["https://www.youtube.com/watch?v=ykY69lSpDdo"]
+
+for url in podcast_url_list:
+    podcast_dataset.add_podcast_from_url(url, podcast_name="Lex Fridman Podcast",
+                                         openai_key="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")
